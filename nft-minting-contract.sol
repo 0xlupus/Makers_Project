9af2ad29391e2d7efa61018893050f3441a12b36
lupus <93690357+0xlupus@users.noreply.github.com>
2022-06-06 15:30:04 -0300

    Update nft-minting-contract.sol

--- nft-minting-contract.sol
+++ nft-minting-contract.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-contract MyNFT is ERC721URIStorage {
+contract NFTProject is ERC721URIStorage {
     using Counters for Counters.Counter;
     
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("MYNFT", "TNFT") {}
+    constructor() ERC721("NFTProject", "PJT") {}
 
     function mintToken(string memory tokenURI)
         public
@@ -23,4 +23,3 @@ contract MyNFT is ERC721URIStorage {
 
         return newItemId;
     }
-}
